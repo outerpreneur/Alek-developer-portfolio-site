2d9f8f19b4b109cfe174de658d71da0469e98421
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2024-11-14 16:48:04 +0500

    slider and tailwind cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,12 @@ module.exports = {
         midnight: "#151515",
         footerbg: "#242424",
         metalgray: "#d9d9d9",
+        orangemetal: "#FFA500",
       },
     },
     fontFamily: {
       spacegrotesk: ["Space grotesk", "sans-serif"],
+      oswald: ["Oswald", "sans-serif"],
     },
     backgroundImage: {
       circlepattern: "url('./assets/images/pattern-circle.svg')",
